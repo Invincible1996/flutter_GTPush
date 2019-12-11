b5b4251553df7e46611b104d067d0834e0d4ea99
wenqiang <717092390@qq.com>
2019-12-11 14:31:46 +0800

    添加图片资源

---
+++ android/src/main/res/flutter_gtpush.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:ffi';
+
+import 'package:flutter/services.dart';
+
+class FlutterGTPush {
+  static const MethodChannel _channel = const MethodChannel('flutter_gtpush');
+
+  static void init() async {
+    await _channel.invokeMethod('init');
+  }
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  /*
+   *获取clientId
+   */
+  static Future<String> get clientId async {
+    final String clientId = await _channel.invokeMethod('getClientId');
+    return clientId;
+  }
+
+  static void showToast() async {
+    await _channel.invokeMethod('showToast');
+//    return clientId;
+  }
+}
